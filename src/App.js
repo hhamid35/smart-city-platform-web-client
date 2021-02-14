2b70ed6f3c2f49ceb08c2e14b56af16a7f7e926c
Hamid Haris <hhamid35@hotmail.com>
2021-02-13 20:02:26 -0500

    testing ci/cd pipeline

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Smart City Platform Capstone Project
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.ee.ryerson.ca/capstone/topics/2020/MJ06.html"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Project Descrtiption
         </a>
       </header>
     </div>
